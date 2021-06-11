ae84cc1162f24b4d3688cad58e68e5588bbdbd79
Samuel Pilay Muñoz <yhau1989@gmail.com>
2021-06-11 17:57:45 -0500

    responsive desing xl size

--- client/labortoria-front/src/pages/Home/index.js
+++ client/labortoria-front/src/pages/Home/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     <>
     <div className="relative bg-white overflow-hidden ">
       <div className="max-w-7xl ">
-        <div className="relative z-10 pb-8 bg-white opacity-100 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="relative z-10 pb-8 bg-white opacity-100 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 xl:max-w-4xl">
           <svg
             className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white  transform translate-x-1/2"
             fill="currentColor"
@@ -126,11 +126,13 @@ export default function Home() {
 
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
+              
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Face{' =) '}Lab</span>{' '}
-                <span className="block text-pink-500 xl:inline">Socal network</span>
+                <span className="block text-pink-500 ">Socal network</span>
               </h1>
-              <p className="mt-3 text-base text-gray-800 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              <br />
+              <p className=" mt-3 text-base text-gray-800 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
               Forget about Facebook, our network is better =) Start making friends
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
@@ -155,7 +157,7 @@ export default function Home() {
           </main>
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-3/4">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
           src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"

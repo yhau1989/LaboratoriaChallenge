c048933f2b48e9867360839e53ba5497627a1ea8
Samuel Pilay Muñoz <yhau1989@gmail.com>
2021-06-13 01:44:11 -0500

    drop console.log from api

--- api/src/routers/posts/index.js
+++ api/src/routers/posts/index.js
@@ -12,11 +12,11 @@ router.get('/posts/*', function(req, res) {
 //get list all post by user id
 router.post("/posts/list/", async(req, res, next) => {
 
-  console.log('/posts/list/ fue llamado')
+  
     const { idUser } = req.body;
     if (idUser) {
       let list = await getList(idUser);
-      console.log('posts/list',list)
+      
        res.status(200).json(list);
       // res.json(list,200)
     } else {
@@ -38,7 +38,7 @@ router.post("/posts/t/:target", async(req, res) => {
 
 //new post
 router.post("/posts/new/", async(req, res) => {
-  console.log(req.body);
+ 
   const result = await newPost(req.body);
   res.status(200).json(result);
 });
